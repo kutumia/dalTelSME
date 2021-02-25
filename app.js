a4731c44f3591d9e1db5f098b21256efae2ed4ae
njayman <najishm828282@gmail.com>
2021-02-25 10:48:04 +0600

    added redis cache storage for session

--- app.js
+++ app.js
@@ -1,48 +1,55 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var session = require('express-session');
-var localStorage=require('local-storage');
-var dotenv= require('dotenv');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var localStorage = require("local-storage");
+var dotenv = require("dotenv");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var upazillaRouter = require('./routes/upazilla');
-var pdRouter = require('./routes/pd');
-var ddRouter = require('./routes/dd');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var upazillaRouter = require("./routes/upazilla");
+var pdRouter = require("./routes/pd");
+var ddRouter = require("./routes/dd");
+const redis = require("redis");
+const session = require("express-session");
 
+let RedisStore = require("connect-redis")(session);
+let redisClient = redis.createClient();
 
-dotenv.config({path:'./.env'});
+dotenv.config({ path: "./.env" });
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(session({ secret: 'keyboard cat',resave: false, saveUninitialized: false, cookie: { maxAge: Date.now() + (30 * 86400 * 1000*10000)  }}))
-app.use(logger('dev'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: Date.now() + 30 * 86400 * 1000 * 10000 },
+  })
+);
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.locals.type = req.session.type;
-  console.log("app.js",req.session.type,res.locals.type)
+  console.log("app.js", req.session.type, res.locals.type);
   res.locals.user_id = req.session.user_id;
   next();
 });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/upazilla', upazillaRouter);
-app.use('/pd', pdRouter);
-app.use('/dd', ddRouter);
-
-
-
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/upazilla", upazillaRouter);
+app.use("/pd", pdRouter);
+app.use("/dd", ddRouter);
 
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
@@ -59,13 +66,14 @@ app.use('/dd', ddRouter);
 //   res.render('error');
 // });
 
-
-
 const db = require("./models");
-db.sequelize.sync().catch(err=>console.log(err.message))
+db.sequelize.sync().catch((err) => console.log(err.message));
 
-db.sequelize.sync({ force: false }).then(() => {
-  console.log("Drop and re-sync db.");
-}).catch(err=>console.log(err.message))
+db.sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("Drop and re-sync db.");
+  })
+  .catch((err) => console.log(err.message));
 
 module.exports = app;
